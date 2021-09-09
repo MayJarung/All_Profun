597a0a83685cf60de1b949ae24a60e1bce2c403b
May Jarung <mayjarung122545@gmail.com>
2021-09-09 22:16:53 +0700

    Create โจทย์รายสัปดาห์5.1.cpp

---
+++ โจทย์รายสัปดาห์5.1.cpp
@@ -0,0 +1,25 @@
+#include<stdio.h>
+#include<math.h>
+int main()
+{
+    int x,y,z;
+    double s,ar;
+    printf("Input x : ");
+    scanf("%d",&x);
+    printf("Input y : ");
+    scanf("%d",&y);
+    printf("Input z : ");
+    scanf("%d",&z);
+    s = (x+y+z)/2;
+    ar = sqrt(s*(s-x)*(s-y)*(s-z));
+    
+    if(ar>0 &&x>0 && y>0 && z>0)
+	{
+        printf("ar = %.2lf\n",ar);    
+    }
+    else
+	{
+    	printf("ar = 0");
+	}
+    return 0;
+}
