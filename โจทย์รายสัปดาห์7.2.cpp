cf32e240fbe71d84b0539eb9daa9a3cc9d35474c
May Jarung <mayjarung122545@gmail.com>
2021-09-29 22:31:29 +0700

    Create โจทย์รายสัปดาห์7.2.cpp

---
+++ โจทย์รายสัปดาห์7.2.cpp
@@ -0,0 +1,32 @@
+#include <stdio.h>  
+int main()  
+{  
+    float t1,t2,t3,t4,t5,t6,t7,t8,t9,t10,everage;
+    
+    printf("Heigt 1 : ");
+    scanf("%f",&t1);
+    printf("Heigt 2 : ");
+    scanf("%f",&t2);
+    printf("Heigt 3 : ");
+    scanf("%f",&t3);
+    printf("Heigt 4 : ");
+    scanf("%f",&t4);
+    printf("Heigt 5 : ");
+    scanf("%f",&t5);
+    printf("Heigt 6 : ");
+    scanf("%f",&t6);
+    printf("Heigt 7 : ");
+    scanf("%f",&t7);
+    printf("Heigt 8 : ");
+    scanf("%f",&t8);
+    printf("Heigt 9 : ");
+    scanf("%f",&t9);
+    printf("Heigt 10 : ");
+    scanf("%f",&t10);
+	  
+    everage = (t1+t2+t3+t4+t5+t6+t7+t8+t9+t10)/10;
+    
+    printf("everage = %.2lf\n",everage);
+    
+    return 0;  
+}  
